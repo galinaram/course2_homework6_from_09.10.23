a624f73b0fa5d7800f8be68f5d3ea333535d2966
galina <galina.ramodina@gmail.com>
2023-10-16 16:39:40 +0300

    есть все исключения

---
+++ src/main/java/course2/homework6/hw6/ArrayIsFull.java
@@ -0,0 +1,7 @@
+package course2.homework6.hw6;
+
+public class ArrayIsFull extends RuntimeException {
+    public ArrayIsFull(){
+        super("ArrayIsFull");
+    }
+}
---
+++ src/main/java/course2/homework6/hw6/EmployeeNotFound.java
@@ -0,0 +1,7 @@
+package course2.homework6.hw6;
+
+public class EmployeeNotFound extends RuntimeException{
+    public EmployeeNotFound (){
+        super("EmployeeNotFound");
+    }
+}
